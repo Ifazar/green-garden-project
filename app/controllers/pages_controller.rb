ce957f7e52b4be73a3bcc31a470f816e08b84a3c
guillaume larzilliere <guillaume3608@gmail.com>
2023-11-20 16:52:21 +0100

    ajout des actions dans le controller pages / index, new, create, params / et ajout de la vue pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,31 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    
   end
+
+  def index
+    @pages = Garden.all
+  end
+
+  def new
+    @page = Garden.new
+  end
+
+  def create
+    @page = Garden.new(garden_params)
+    if @page.save
+    redirect_to garden_path(@page)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  private
+
+
+    def garden_params
+        params.require(:garden).permit(:name, :address, :detail, :tool, :picture, :price)
+    end
 end
+
+
