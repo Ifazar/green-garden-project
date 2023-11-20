9bf6804e595c89ae7aa40ab188a8e08fd5a3f0d5
RAZAFIMANOHIHAJA Aurelien <aurelienrazafi@gmail.com>
2023-11-20 12:29:46 +0100

    models creation

---
+++ db/migrate/20231120112233_create_gardens.rb
@@ -0,0 +1,15 @@
+class CreateGardens < ActiveRecord::Migration[7.0]
+  def change
+    create_table :gardens do |t|
+      t.string :name
+      t.string :address
+      t.string :detail
+      t.string :tool
+      t.string :picture
+      t.integer :price
+      t.date :date
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,50 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2023_11_20_112549) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "gardens", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "detail"
+    t.string "tool"
+    t.string "picture"
+    t.integer "price"
+    t.date "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "garden_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["garden_id"], name: "index_reservations_on_garden_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "address"
+    t.string "email"
+    t.string "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "reservations", "gardens"
+  add_foreign_key "reservations", "users"
+end
