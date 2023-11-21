e797d5cdc4e1748951fdccbec70b7c0254220ab9
Francois Villalba <villalbajunior@msn.com>
2023-11-21 17:12:58 +0100

    validations en cours, ne fonctionne pas

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,2 +1,10 @@
 class Garden < ApplicationRecord
+  has_many :reservations
+  belongs_to :user
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :detail, presence: true
+  validates :price, presence: true
+
 end
