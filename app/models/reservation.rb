b24af7d8053bb54d38862505143ef4d3d647e226
Francois Villalba <villalbajunior@msn.com>
2023-11-22 11:42:07 +0100

    Validates presque fini + reservations presque fini

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,11 +1,7 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :garden
-  
-  validates :date, presence: true
-  # validates :user_id, presence: true, if: :same_user?
-  # def same_user?
-  #   :user_id != :garden_id_user_id
-  # end
+
+  # validates :date, presence: true
 
 end
