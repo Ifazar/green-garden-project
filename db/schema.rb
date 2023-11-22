da12232c5816edb8cfadd7416e8a69529aec30b7
RAZAFIMANOHIHAJA Aurelien <aurelienrazafi@gmail.com>
2023-11-22 10:51:56 +0100

    ajout colonne user_id to Gardens

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_21_145134) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_22_094542) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_21_145134) do
     t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_gardens_on_user_id"
   end
 
   create_table "reservations", force: :cascade do |t|
